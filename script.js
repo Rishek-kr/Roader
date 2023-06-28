84f5116ee15265d44e1a0318cb7a4a8c5a1b3f43
Rishek Kumar <98318051+Rishek-kr@users.noreply.github.com>
2023-06-29 00:15:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+gsap.from(".img h4",{
+    opacity:0,
+    duration:2,
+    y:60
+})
+gsap.from("form",{
+    opacity:0,
+    duration:2,
+    y:90
+})
+gsap.from("h3",{
+    opacity:0,
+    duration:3,
+    x:40
+})
+gsap.from("h1",{
+    opacity:0,
+    duration:3,
+    x:-60
+})
+gsap.from(".footer h4",{
+    opacity:-1,
+    duration:3,
+})
+gsap.from("a",{
+    opacity:-2,
+    duration:3,
+})
